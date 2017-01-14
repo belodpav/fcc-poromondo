77333cb63ffef85fad6544d165284418e7254aaf
Pavel Belodurin <belod.pav@gmail.com>
2017-01-14 14:02:58 +0300

    added clock loop functionality

--- assets/js/poromondo-core.js
+++ assets/js/poromondo-core.js
@@ -30,14 +30,15 @@ function poromondoClock(breakLength, sessionLength, currentMode) {
     },
     this.timerTimedCount = function (displayRef, time) {
         var thisVal = this; // Let's save reference to this 
-        while (time > 0 && this.currentTimerState === 1) {
-        setTimeout(function () {
-            time -=1;
-            displayRef.innerHTML = time;
-            thisVal.setCurrenTimerValue(time);
-            console.log(time);
-        }, 50);
-        } /*if (time === 0) {
+        if (time > 0 && this.currentTimerState === 1) {
+        time -=1;
+        displayRef.innerHTML = time;
+        this.setCurrenTimerValue(time);
+        console.log(time);
+        setTimeout(function () {thisVal.timerTimedCount(displayRef, time);}, 50);
+        
+        } if (time === 0) {
+            this.currentTimerState = 0;
             console.log("!!!=====  ", this.currentMode ,"  ======!!!");
             switch (this.currentMode) {
                 case 0:
@@ -52,8 +53,9 @@ function poromondoClock(breakLength, sessionLength, currentMode) {
                     this.setCurrenTimerValue(time);
                     this.timerTimedCount(displayRef, this.getCurrentTimerValue());
                     break;
-                    
+                  
             }
+             this.currentTimerState = 1; 
             /*if (this.currentMode === 0) {
                 
                 this.setCurrenTimerValue(60*this.breakLength);
@@ -64,9 +66,9 @@ function poromondoClock(breakLength, sessionLength, currentMode) {
                 this.setCurrenTimerValue(60*this.sessionLength);
                 //this.timerTimedCount(displayRef, this.getCurrentTimerValue());
                 //this.currentMode = 0;
-            }
+            }*/
             
-        }*/
+        }
     },
     this.getCurrentTimerValue = function () {
         return this.currentTimerValue;
